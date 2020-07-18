713a181324e3eced5a81a19572eeac13327945d1
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2020-07-18 16:28:30 +0200

    sob, 18 lip 2020, 16:28:30 CEST

--- pyiot/base.py
+++ pyiot/base.py
@@ -1,26 +1,68 @@
 from typing import Set, Any, Dict
 from pyiot.traits import Trait
 
+
 class DeviceStatus:
     def __init__(self) -> None:
         self._data:Dict[str, Any]
     
+    def register_property(self, property_name:str, property_name_setter:str):
+        pass
     
-
+    def update(self, value:Dict[str,Any]):
+        pass
+    
+    def get(self, name:str) -> str:
+        return getattr(self, name, default="")
+    
+    def set(self, name:str, value:str):
+        if hasattr(self, name):
+            setattr(self, name, value)    
+    
+    @property
+    def sid(self):
+        return self.get("sid")
+    
+    @property
+    def name(self):
+        return self.get('name')
+    
+    @name.setter
+    def name(self, value:str):
+        self.set('name', value)
+    
+    @property
+    def place(self):
+        return self.get('place')
+    
+    @place.setter
+    def place(self, value:str):
+        self.set('place', value)
+    
+    @property
+    def model(self):
+        return self.get('model')
+    
+    def device_status(self) -> Dict[str, str]:
+        return {"sid": self.sid, "name": self.name, "place": self.place}
+    
+    
 class BaseDevice:
     def __new__(cls):
         cls._traits:Set[str] = set()
         cls._cmds:Set[str] = set()
+        cls.status:DeviceStatus = DeviceStatus()
         for _base_class in cls.__bases__:
             _name: str = _base_class.__name__
             if issubclass(_base_class, Trait):
                 cls.traits.add(_name)
                 cls._cmds.update(_base_class._commands)
+                for _prop in _base_class.properyties:
+                    cls.status.register_property(_prop)
         
         return super(BaseDevice, cls).__new__(cls)
     
     def __init__(self, sid:str) -> None:
-        self._data:DeviceStatus = DeviceStatus()
     
         print('init Basedev')
     
@@ -38,7 +80,7 @@ class BaseDevice:
             _cmd(**kwargs)
                 
     def query(self, name:str) -> Any:
-        self._data.get(name, '')
+        return self.status.get(name)
     
     
     @classmethod    
--- pyiot/traits.py
+++ pyiot/traits.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Any, Set
+from typing import Any, Set, Dict
 
 
 class Trait(ABC):
@@ -9,6 +9,7 @@ class Trait(ABC):
 
 class OnOff(Trait):
     _commands:Set[str] = {'on', 'off'}
+    _propertyies: Dict[str, str] = {'power':'power'}
     
     @abstractmethod
     def on(self, **kwargs:Any) -> None:
@@ -18,14 +19,23 @@ class OnOff(Trait):
     @abstractmethod
     def off(self, **kwargs:Any) -> None:
         pass
-
-    
-class Dimmer(Trait):
-    _commands:Set[str] = {'dimm'}
     
     @abstractmethod
-    def dimm(self):
+    def is_on(self) -> bool:
         pass
+    
+    @abstractmethod
+    def is_off(self) -> bool:
+        pass
+    
+class Dimmer(Trait):
+    _commands:Set[str] = {'set_bright'}
+    _property: Dict[str, str] = {'bright':'bright'}
+    
+    @abstractmethod
+    def set_bright(self):
+        pass
+    
 
 class Toggle(Trait):
     def toggle(self) -> None:
