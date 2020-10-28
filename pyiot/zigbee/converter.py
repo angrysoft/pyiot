12c2c03def9804bd3d02f9fd75f0bd67faa8b536
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2020-10-28 10:23:02 +0100

    śro, 28 paź 2020, 10:23:02 CET

---
+++ pyiot/zigbee/converter.py
@@ -0,0 +1,14 @@
+from typing import Dict, Any
+
+
+class Converter:
+    def __init__(self) -> None:
+        pass
+    
+    def add_device(self, moodel: str, ):pass
+    
+    def to_gateway(self) -> Dict[str, Any]:
+        return {}
+    
+    def to_device(self) -> Dict[str, Any]:
+        return {}
