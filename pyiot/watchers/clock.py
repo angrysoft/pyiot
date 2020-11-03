d03a6fba2e6465f1dd2e08573d921210205a5225
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2020-11-03 21:11:58 +0100

    wto, 3 lis 2020, 21:11:58 CET

---
+++ pyiot/watchers/clock.py
@@ -0,0 +1,36 @@
+from time import sleep
+from . import WatcherBaseDriver
+from pyiot import BaseDevice
+from pyiot.virtual.clock import Time
+from typing import Callable, Optional, Dict, Any
+
+class BraviaWatcher(WatcherBaseDriver):
+    def __init__(self, sleep_time: int, device: Optional[BaseDevice] = None) -> None:
+        self.sleep_time = 60
+        self._loop = True
+        self.device = device
+
+    def watch(self, handler: Callable[[Optional[Dict[str, Any]]], None]) -> None:
+        while datetime.now().second:
+            sleep(1)
+                          
+        
+        while self._loop:
+            _sunset :bool = False
+            _sunrise: bool = False
+            _time: Time = Time()
+            _time.set_now()
+            
+            self.device.status.time = _time
+            if _time == self.device.status.sunrise:
+                _sunrise = True
+            elif _time == self.device.status.sunset:
+                _sunset = True
+            elif _time  == Time(1):
+                self.device.sun_info()
+            
+            handler({'cmd': 'report', 'sid': self.device.status.sid, 'data': {'time': _time, 'sunrise': _sunrise, 'sunset': _sunset}})
+            sleep(60)
+    
+    def stop(self) -> None:
+        self._loop = False
---
+++ tests/test_traits.py
@@ -0,0 +1,27 @@
+import unittest
+from pyiot.traits import *
+from pyiot import BaseDevice
+
+class Dev(BaseDevice, MutliSwitch):
+    def __init__(self) -> None:
+        self.status.switches = ['left', 'right']
+    
+    def on(self, switch_name: str):
+        pass
+    
+    def off(self, switch_name: str):
+        pass
+    
+    def is_off(self, switch_name: str) -> bool:
+        return True
+    
+    def is_on(self, switch_name: str) -> bool:
+        return True
+
+class TestTraits(unittest.TestCase):
+    def test_class_instace(self):
+        c = Dev('ccccc')
+        d = Dev('ddddd')
+        print(c.status.switches)
+        print(d.status.switches)
+        
