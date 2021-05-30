7ae346a8f77f7e004271991128b78f64658750c9
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2021-05-30 12:08:34 +0200

    nie, 30 maj 2021, 12:08:34 CEST

--- pyiot/zigbee/zigbee2mqtt.py
+++ pyiot/zigbee/zigbee2mqtt.py
@@ -72,7 +72,7 @@ class Zigbee2mqttGateway(ZigbeeGateway):
         self._converter.add_device(device.status.model, payloads.get(device.status.model, {}))
         # payload: Dict[str, str] = payloads.get(device.status.model, {})
         # for x in payload.values():
-        self._client.publish(f"zigbee2mqtt/{device.status.sid}/get", f'{{"linkquality":""}}')
+        # self._client.publish(f"zigbee2mqtt/{device.status.sid}/get", f'{{"linkquality":""}}')
     
     def unregister_sub_device(self, device_id: str) -> None:
         self.del_topic(f"zigbee2mqtt/{device_id}")
