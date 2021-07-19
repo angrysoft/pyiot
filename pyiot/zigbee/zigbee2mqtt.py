3a63fedefe710343542bc65c35f95eaae38034f9
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2021-07-19 12:25:17 +0200

    pon, 19 lip 2021, 12:25:17 CEST

--- pyiot/zigbee/zigbee2mqtt.py
+++ pyiot/zigbee/zigbee2mqtt.py
@@ -64,8 +64,8 @@ class Zigbee2mqttGateway(ZigbeeGateway):
         self.add_topic(f"zigbee2mqtt/{device.status.sid}")
         self._converter.add_device(device.status.model, payloads.get(device.status.model, {}))
         # payload: Dict[str, str] = payloads.get(device.status.model, {})
-        # for x in payload.values():
-        # self._client.publish(f"zigbee2mqtt/{device.status.sid}/get", f'{{"":""}}')
+        for x in payload.get('device_statuses', []):
+            self._client.publish(f"zigbee2mqtt/{device.status.sid}/get", f'{{${x}:""}}')
     
     def unregister_sub_device(self, device_id: str) -> None:
         self.del_topic(f"zigbee2mqtt/{device_id}")
@@ -79,8 +79,8 @@ class Zigbee2mqttGateway(ZigbeeGateway):
 
 # device model : device : gateway
 payloads = {
-    'ctrl_neutral1': {'power': 'state', 'linkquality': 'linkquality'},
-    'ctrl_neutral2': {'left': 'state_left', 'right': 'state_right', 'linkquality': 'linkquality'},
+    'ctrl_neutral1': {'power': 'state', 'linkquality': 'linkquality', 'device_statuses': ['state']},
+    'ctrl_neutral2': {'left': 'state_left', 'right': 'state_right', 'linkquality': 'linkquality', 'device_statuses': ['state_left', 'state_right']},
     'plug': {'power': 'state', 'power_consumed': 'consumption', 'linkquality': 'linkquality', 'load_power': 'power', 'toggle': 'toggle'},
     'magnet': {'status': 'contact'},
     'weather.v1': {'temperature': 'temperature', 'humidity': 'humidity'},
